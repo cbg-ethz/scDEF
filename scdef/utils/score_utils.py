d72cc8df33ef5ed62ed22b7fe3cd0709c428b875
pedrofale <pedro.ferreira@bsse.ethz.ch>
2023-07-27 12:57:36 +0100

    Add Gini to utilities

--- scdef/utils/score_utils.py
+++ scdef/utils/score_utils.py
@@ -140,3 +140,17 @@ def compute_cluster_obs_association_score(
         cells_in_factor_not_from_obs,
         cells_not_in_factor_from_obs,
     )
+
+
+def gini(x):
+    # (Warning: This is a concise implementation, but it is O(n**2)
+    # in time and memory, where n = len(x).  *Don't* pass in huge
+    # samples!)
+
+    # Mean absolute difference
+    mad = np.abs(np.subtract.outer(x, x)).mean()
+    # Relative mean absolute difference
+    rmad = mad / np.mean(x)
+    # Gini coefficient
+    g = 0.5 * rmad
+    return g
