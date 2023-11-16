a062925e7db95ebfd260cc06628b4636f8e83e83
pedrofale <pedro.ferreira@bsse.ethz.ch>
2023-11-16 12:24:58 +0100

    Include more genes in test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -84,7 +84,7 @@ def test_scdef():
     sc.pp.normalize_total(adata, target_sum=1e4)
     sc.pp.log1p(adata)
     sc.pp.highly_variable_genes(
-        adata, min_mean=0.0125, max_mean=3, min_disp=0.5, n_top_genes=200
+        adata, min_mean=0.0125, max_mean=3, min_disp=0.5, n_top_genes=300
     )
     raw_adata = raw_adata[:, adata.var.highly_variable]
     adata = adata[:, adata.var.highly_variable]
