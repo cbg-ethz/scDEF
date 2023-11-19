b4fb9de71af1bc6ad026c094e4be1bc3a756c9ae
pedrofale <pedro.ferreira@bsse.ethz.ch>
2023-11-19 18:43:51 +0100

    Lint scripts

--- simulations/workflow/scripts/run_scdef_un.py
+++ simulations/workflow/scripts/run_scdef_un.py
@@ -10,7 +10,11 @@ counts = pd.read_csv(snakemake.input["counts_fname"], index_col=0)
 meta = pd.read_csv(snakemake.input["meta_fname"])
 markers = pd.read_csv(snakemake.input["markers_fname"])
 
-markers['cluster'] = (markers['cluster'].apply(lambda row: row.split("Group")[1]).astype(int)-1).astype('str').astype('category')
+markers["cluster"] = (
+    (markers["cluster"].apply(lambda row: row.split("Group")[1]).astype(int) - 1)
+    .astype("str")
+    .astype("category")
+)
 groups = markers["cluster"].unique()
 markers = dict(
     zip(groups, [markers.loc[markers["cluster"] == g]["gene"].tolist() for g in groups])
@@ -40,13 +44,13 @@ adata.obs["GroupA"] = (
     .astype("str")
     .astype("category")
 )
-adata.obs['GroupA'] = adata.obs['GroupA'].apply(lambda row: f'hh{row}')
+adata.obs["GroupA"] = adata.obs["GroupA"].apply(lambda row: f"hh{row}")
 adata.obs["GroupB"] = (
     (adata.obs["GroupB"].apply(lambda row: row.split("Group")[1]).astype(int) - 1)
     .astype("str")
     .astype("category")
 )
-adata.obs['GroupB'] = adata.obs['GroupB'].apply(lambda row: f'h{row}')
+adata.obs["GroupB"] = adata.obs["GroupB"].apply(lambda row: f"h{row}")
 adata.obs["GroupC"] = (
     (adata.obs["GroupC"].apply(lambda row: row.split("Group")[1]).astype(int) - 1)
     .astype("str")
@@ -63,7 +67,7 @@ metrics_list = [
     "Cell Type ASW",
     "Batch ARI",
     "Batch ASW",
-    "Hierarchy accuracy",    
+    "Hierarchy accuracy",
     "Hierarchical signature consistency",
     "Signature sparsity",
     "Signature accuracy",
